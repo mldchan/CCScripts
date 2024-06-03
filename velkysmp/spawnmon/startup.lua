258ca46af8ebefc5746f7abff46de9bd6ffcea62
Akatsuki <akatsuki@noreply.codeberg.org>
2024-06-03 21:06:12 +0200

    Download raw content instead of website

--- velkysmp/spawnmon/startup.lua
+++ velkysmp/spawnmon/startup.lua
@@ -3,11 +3,11 @@ peripheral.call("right", "clear")
 
 print("Checking updates...")
 fs.delete("startup.lua")
-shell.run("wget https://codeberg.org/Akatsuki/ComputerCraftScripts/src/branch/main/velkysmp/spawnmon/startup.lua startup.lua")
+shell.run("wget https://codeberg.org/Akatsuki/ComputerCraftScripts/raw/branch/main/velkysmp/spawnmon/startup.lua startup.lua")
 fs.delete("json.lua")
 shell.run("wget https://raw.githubusercontent.com/rxi/json.lua/master/json.lua json.lua")
 fs.delete("main.lua")
-shell.run("wget https://codeberg.org/Akatsuki/ComputerCraftScripts/src/branch/main/velkysmp/spawnmon/main.lua main.lua")
+shell.run("wget https://codeberg.org/Akatsuki/ComputerCraftScripts/raw/branch/main/velkysmp/spawnmon/main.lua main.lua")
 
 term.clear()
 
