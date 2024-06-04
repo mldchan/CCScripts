ab7c8f8765233e6bfa2063d7368f64887a3eca4e
Akatsuki <akatsuki@noreply.codeberg.org>
2024-06-04 19:39:36 +0200

    Reactivate le thingy

--- velkysmp/spawnmon/startup.lua
+++ velkysmp/spawnmon/startup.lua
@@ -1,39 +1,39 @@
--- -- wget run https://codeberg.org/Akatsuki/ComputerCraftScripts/raw/branch/main/velkysmp/spawnmon/startup.lua
--- peripheral.call("right", "clear")
+-- wget run https://codeberg.org/Akatsuki/ComputerCraftScripts/raw/branch/main/velkysmp/spawnmon/startup.lua
+peripheral.call("right", "clear")
 
--- print("Checking updates...")
--- fs.delete("startup.lua")
--- shell.run("wget https://codeberg.org/Akatsuki/ComputerCraftScripts/raw/branch/main/velkysmp/spawnmon/startup.lua startup.lua")
--- fs.delete("json.lua")
--- shell.run("wget https://raw.githubusercontent.com/rxi/json.lua/master/json.lua json.lua")
--- fs.delete("main.lua")
--- shell.run("wget https://codeberg.org/Akatsuki/ComputerCraftScripts/raw/branch/main/velkysmp/spawnmon/main.lua main.lua")
--- fs.delete("utils.lua")
--- shell.run("wget https://codeberg.org/Akatsuki/ComputerCraftScripts/raw/branch/main/velkysmp/utils.lua utils.lua")
+print("Checking updates...")
+fs.delete("startup.lua")
+shell.run("wget https://codeberg.org/Akatsuki/ComputerCraftScripts/raw/branch/main/velkysmp/spawnmon/startup.lua startup.lua")
+fs.delete("json.lua")
+shell.run("wget https://raw.githubusercontent.com/rxi/json.lua/master/json.lua json.lua")
+fs.delete("main.lua")
+shell.run("wget https://codeberg.org/Akatsuki/ComputerCraftScripts/raw/branch/main/velkysmp/spawnmon/main.lua main.lua")
+fs.delete("utils.lua")
+shell.run("wget https://codeberg.org/Akatsuki/ComputerCraftScripts/raw/branch/main/velkysmp/utils.lua utils.lua")
 
--- term.clear()
+term.clear()
 
--- print("Running main.lua...")
--- shell.run("main.lua")
+print("Running main.lua...")
+shell.run("main.lua")
 
--- require("utils")
--- local json = require("json")
--- local config = json.decode(readFile("config.json"))
+require("utils")
+local json = require("json")
+local config = json.decode(readFile("config.json"))
 
--- prettyWrite(term, "Nice try. I see you.")
--- print()
+prettyWrite(term, "Nice try. I see you.")
+print()
 
--- http.post(config.webhook, json.encode({
---     content = "Computer " .. os.getComputerID() .. " had it's program terminated! <@" .. config.userId .. ">"
--- }), {
---     ["Content-Type"] = "application/json"
--- })
+http.post(config.webhook, json.encode({
+    content = "Computer " .. os.getComputerID() .. " had it's program terminated! <@" .. config.userId .. ">"
+}), {
+    ["Content-Type"] = "application/json"
+})
 
--- prettyWrite(term, "Akatsuki was alerted. Please don't try this again.")
--- print()
--- prettyWrite(term, "Unless you just wanted to update the program. In that case, please wait a moment.")
--- print()
+prettyWrite(term, "Akatsuki was alerted. Please don't try this again.")
+print()
+prettyWrite(term, "Unless you just wanted to update the program. In that case, please wait a moment.")
+print()
 
--- os.sleep(1)
+os.sleep(1)
 
--- os.reboot()
+os.reboot()
