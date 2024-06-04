eb3ea16ed4f37d15b65224bbe9c90c83c1d5362d
Akatsuki <akatsuki@noreply.codeberg.org>
2024-06-04 19:45:40 +0200

    Temporarily disable startup script again

--- velkysmp/protection/startup.lua
+++ velkysmp/protection/startup.lua
@@ -1,34 +1,34 @@
-settings.set("shell.allow_disk_startup", false)
+-- settings.set("shell.allow_disk_startup", false)
 
-fs.delete("startup.lua")
-shell.run("wget https://codeberg.org/Akatsuki/ComputerCraftScripts/raw/branch/main/velkysmp/protection/startup.lua startup.lua")
-fs.delete("json.lua")
-shell.run("wget https://raw.githubusercontent.com/rxi/json.lua/master/json.lua json.lua")
-fs.delete("main.lua")
-shell.run("wget https://codeberg.org/Akatsuki/ComputerCraftScripts/raw/branch/main/velkysmp/protection/main.lua main.lua")
-fs.delete("utils.lua")
-shell.run("wget https://codeberg.org/Akatsuki/ComputerCraftScripts/raw/branch/main/velkysmp/utils.lua utils.lua")
+-- fs.delete("startup.lua")
+-- shell.run("wget https://codeberg.org/Akatsuki/ComputerCraftScripts/raw/branch/main/velkysmp/protection/startup.lua startup.lua")
+-- fs.delete("json.lua")
+-- shell.run("wget https://raw.githubusercontent.com/rxi/json.lua/master/json.lua json.lua")
+-- fs.delete("main.lua")
+-- shell.run("wget https://codeberg.org/Akatsuki/ComputerCraftScripts/raw/branch/main/velkysmp/protection/main.lua main.lua")
+-- fs.delete("utils.lua")
+-- shell.run("wget https://codeberg.org/Akatsuki/ComputerCraftScripts/raw/branch/main/velkysmp/utils.lua utils.lua")
 
-shell.run("main.lua")
+-- shell.run("main.lua")
 
-require("utils")
-local json = require("json")
+-- require("utils")
+-- local json = require("json")
 
-prettyWrite(term, "Nice try. I see you.")
+-- prettyWrite(term, "Nice try. I see you.")
 
-local config = json.decode(readFile("config.json"))
+-- local config = json.decode(readFile("config.json"))
 
-http.post(config.webhook, json.encode({
-    content = "Computer " .. os.getComputerID() .. " had it's program terminated! <@" .. config.userId .. ">"
-}), {
-    ["Content-Type"] = "application/json"
-})
+-- http.post(config.webhook, json.encode({
+--     content = "Computer " .. os.getComputerID() .. " had it's program terminated! <@" .. config.userId .. ">"
+-- }), {
+--     ["Content-Type"] = "application/json"
+-- })
 
-prettyWrite(term, "Akatsuki was alerted. Please don't try this again.")
-print()
-prettyWrite(term, "Unless you just wanted to update the program. In that case, please wait a moment.")
-print()
+-- prettyWrite(term, "Akatsuki was alerted. Please don't try this again.")
+-- print()
+-- prettyWrite(term, "Unless you just wanted to update the program. In that case, please wait a moment.")
+-- print()
 
-os.sleep(1)
+-- os.sleep(1)
 
-os.reboot()
+-- os.reboot()
