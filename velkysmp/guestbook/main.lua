46c9a4c652bed9add5ae948083d6049e3efff5fe
Akatsuki <akatsuki@noreply.codeberg.org>
2024-06-05 16:23:31 +0200

    Management test 1

--- velkysmp/guestbook/main.lua
+++ velkysmp/guestbook/main.lua
@@ -10,6 +10,8 @@ end
 local screen = "main"
 local term_width, term_height = term.getSize()
 
+local managementIndex = 1
+
 function drawScreen()
     term.clear()
 
@@ -32,6 +34,9 @@ function drawScreen()
         term.setCursorPos(2, term_height - 1)
         term.write("[ Sign ]")
 
+        term.setCursorPos(11, term_height - 1)
+        term.write("[ Management ]")
+
         local time = os.time()
         local dayUnfinished = os.day()
         local year = math.floor(dayUnfinished / 365) + 1
@@ -48,7 +53,9 @@ function drawScreen()
         minutes = string.format("%02d", minutes)
 
         term.setCursorPos(1, 1)
-        term.write(tostring(year) .. "/" .. tostring(month) .. "/" .. tostring(day) .. " " .. tostring(hours) .. ":" .. tostring(minutes) .. " -- MC time!")
+        term.write(tostring(year) ..
+        "/" ..
+        tostring(month) .. "/" .. tostring(day) .. " " .. tostring(hours) .. ":" .. tostring(minutes) .. " -- MC time!")
     elseif screen == "sign" then
         term.setCursorPos(2, 2)
         prettyWrite(term, "Please enter the title of your entry.")
@@ -74,6 +81,45 @@ function drawScreen()
         writeFile("guestbook.json", json.encode(guestbookEntries))
         screen = "main"
         drawScreen()
+    elseif screen == "enter_password" then
+        term.setCursorPos(2, 2)
+        prettyWrite(term, "Please enter the password to access the management.")
+
+        os.loadAPI("keyboard")
+        local password = keyboard.inputKeyboard()
+        os.unloadAPI("keyboard")
+
+        if password == config.password then
+            screen = "management"
+            drawScreen()
+        else
+            screen = "main"
+            drawScreen()
+        end
+    elseif screen == "management" then
+        
+        term.setCursorPos(2, 2)
+        prettyWrite(term, "Guestbook Management")
+
+        local entryTitle = guestbookEntries[managementIndex].title
+        local entryContent = guestbookEntries[managementIndex].content
+        term.setCursorPos(2, 4)
+        prettyWrite(term, "Title: " .. entryTitle)
+        term.setCursorPos(2, 5)
+        prettyWrite(term, "Content: " .. entryContent)
+
+        term.setCursorPos(2, 7)
+        prettyWrite(term, "[ Delete ]")
+
+        if managementIndex ~= 1 then
+            term.setCursorPos(2, 9)
+            prettyWrite(term, "[ Previous ]")
+        end
+
+        if managementIndex ~= #guestbookEntries - 1 then
+            term.setCursorPos(15, 9)
+            prettyWrite(term, "[ Next ]")
+        end
     end
 end
 
@@ -89,6 +135,29 @@ while true do
                 screen = "sign"
                 drawScreen()
             end
+
+            if p2 > 11 and p2 < 23 and p3 == term_height - 1 then
+                screen = "enter_password"
+                drawScreen()
+            end
+        elseif screen == "management" then
+
+            if p2 > 1 and p2 < 13 and p3 == 7 then
+                table.remove(guestbookEntries, managementIndex)
+                writeFile("guestbook.json", json.encode(guestbookEntries))
+                drawScreen()
+            end
+
+            if p2 > 1 and p2 < 14 and p3 == 9 then
+                managementIndex = managementIndex - 1
+                drawScreen()
+            end
+
+            if p2 > 14 and p2 < 22 and p3 == 9 then
+                managementIndex = managementIndex + 1
+                drawScreen()
+            end
+
         end
     end
 
