63702a961d0953a3d40bd8831336f4c294426ee6
Akatsuki <akatsuki@noreply.codeberg.org>
2024-06-05 17:28:53 +0200

    Delete old files

--- velkysmp/guestbook/startup.lua
+++ velkysmp/guestbook/startup.lua
@@ -34,6 +34,7 @@ fs.delete("main.lua")
 shell.run("wget https://codeberg.org/Akatsuki/ComputerCraftScripts/raw/branch/main/velkysmp/guestbook/main.lua main.lua")
 fs.delete("utils.lua")
 shell.run("wget https://codeberg.org/Akatsuki/ComputerCraftScripts/raw/branch/main/velkysmp/utils.lua utils.lua")
+fs.delete("keyboard")
 fs.delete("keyboard.lua")
 shell.run("wget https://codeberg.org/Akatsuki/ComputerCraftScripts/raw/branch/main/velkysmp/guestbook/keyboard.lua keyboard.lua")
 
