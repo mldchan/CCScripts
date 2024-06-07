75a8fc8c71f87f51e8f157744e039a62b6317d1a
Akatsuki2555 <akatsuki@noreply.codeberg.org>
2024-06-07 20:21:54 +0200

    slower speed

--- velkysmp/player/spinningfish.lua
+++ velkysmp/player/spinningfish.lua
@@ -2,6 +2,7 @@
 
 local con = peripheral.wrap("back")
 local can = con.canvas3d()
+can.clear()
 can = can.create({x=0,y=1,z=0})
 
 fish = can.addItem({x=0,y=0,z=0}, "minecraft:fish")
@@ -10,7 +11,7 @@ local rot = 0
 
 while true do
   os.sleep(0.05)
-  rot = rot + 36
+  rot = rot + 12
   fish.setRotation(0, rot, 0)
 end
 
