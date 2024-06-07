b38e475c0b2185778afe753275fd457a9ba11e82
Akatsuki2555 <akatsuki@noreply.codeberg.org>
2024-06-07 20:28:41 +0200

    spinning fish above other player's heads

--- velkysmp/player/entdisplay.lua
+++ velkysmp/player/entdisplay.lua
@@ -2,19 +2,21 @@
 local con = peripheral.wrap("back")
 local can = con.canvas()
 
-local ents = can.addText({x=4, y=4}, "")
+can.clear()
+
+local ents = can.addText({x=4,y=4}, "")
 
 while true do
   os.sleep(1)
 
-  local scan = can.sense()
+  local scan = con.sense()
   local scanReady = {}
 
   for _, value in pairs(scan) do
     if scanReady[value.name] == nil then
       scanReady[value.name] = 1
     else
-      scanReady[vale.name] = scanReady[value.name] + 1
+      scanReady[value.name] = scanReady[value.name] + 1
     end
   end
 
--- velkysmp/player/spinningfish.lua
+++ velkysmp/player/spinningfish.lua
@@ -1,17 +1,44 @@
+-- ComputerCraftScripts
+-- Copyright (C) 2024  Akatsuki
 
+-- This program is free software: you can redistribute it and/or modify it under the terms of the
+-- GNU General Public License as published by the Free Software Foundation, either version 3 of
+-- the License, or (at your option) any later version.
+
+-- This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+-- even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+
+-- You should have received a copy of the GNU General Public License along with this program.
+-- If not, see <https://www.gnu.org/licenses/>.
 
 local con = peripheral.wrap("back")
 local can = con.canvas3d()
-can.clear()
-can = can.create({x=0,y=1,z=0})
-
-fish = can.addItem({x=0,y=0,z=0}, "minecraft:fish")
-
 local rot = 0
 
 while true do
   os.sleep(0.05)
   rot = rot + 12
-  fish.setRotation(0, rot, 0)
+
+  local mobsScan = con.sense()
+  local mobs = {}
+
+  for _, value in ipairs(mobsScan) do
+    if value.id ~= nil then
+      local detail = con.getMetaByID(value.id)
+      if detail ~= nil then
+        if detail.food ~= nil then
+          table.insert(mobs, value)
+        end
+      end
+    end
+  end
+
+  for _, value in ipairs(mobs) do
+    local can3d = can.create({x=value.x,y=value.y,z=value.z})
+    local fish = can3d.addItem({x=0,y=1,z=0}, "minecraft:fish")
+
+    fish.setRotation(0, rot, 0)
+  end
 end
 
