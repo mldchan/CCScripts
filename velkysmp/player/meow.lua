ae43e622da1c89ef255cafe6a272c6d20e86ea98
Akatsuki2555 <akatsuki@noreply.codeberg.org>
2024-06-08 09:15:41 +0200

    add debug to meower

--- velkysmp/player/meow.lua
+++ velkysmp/player/meow.lua
@@ -5,12 +5,13 @@ local con = peripheral.wrap("back")
 
 while true do
   os.queueEvent("tick")
-  evt, p1, p2, p3, p4, p5 = os.pullEvent()
+  local evt, p1, p2, p3, p4, p5 = os.pullEvent()
 
   if timer ~= os.time("utc") then
     timer = os.time("utc")
 
-    if math.random(1000) == 1000 then
+    local random = math.random(1000)
+    if random == 1000 then
       local choices = {"Meow :3", "Meow", "Nya", "Nya :3"}
       con.say(choices[math.random(#choices)])
     end
