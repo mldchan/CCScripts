66020e66368ee42ebb4e579e7f97128a17d83683
Akatsuki2555 <akatsuki@noreply.codeberg.org>
2024-06-08 16:36:18 +0200

    fix running of programs

--- velkysmp/player/startup.lua
+++ velkysmp/player/startup.lua
@@ -4,6 +4,8 @@ con = peripheral.wrap("back")
 function updateLib()
   fs.delete("akatsuki.lua")
   shell.run("wget https://codeberg.org/Akatsuki/ComputerCraftScripts/raw/branch/main/velkysmp/player/akatsuki.lua")
+  fs.delete("startup.lua")
+  shell.run("wget https://codeberg.org/Akatsuki/ComputerCraftScripts/raw/branch/main/velkysmp/player/startup.lua")
 end
 
 con.tell("Starting AkatsukiOS...")
@@ -15,17 +17,19 @@ con.tell("Checking for updates and downloading them...")
 checkForUpdates()
 con.tell("Checking for updates was finished.")
 
+local bg = shell.resolveProgram("bg")
+
 -- Meowing
 con.tell("[Starting] Automatic meowing")
-shell.run("bg meow")
+shell.run(bg .. "meow")
 con.tell("[Started] Automatic meowing")
 
 -- Entity display
 con.tell("[Starting] Entity display list")
-shell.run("bg entdisplay")
+shell.run(bg .. "entdisplay")
 con.tell("[Started] Entity display list")
 
 -- Spinning fish :3 
 con.tell("[Starting] Spinning fish")
-shell.run("bg spinningfish")
+shell.run(bg .. "spinningfish")
 con.tell("[Started] Spinning fish")
