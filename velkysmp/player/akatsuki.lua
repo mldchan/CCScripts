d6cc3baa2cb79e54166574290be7ca997278d745
Akatsuki2555 <akatsuki@noreply.codeberg.org>
2024-06-08 16:41:54 +0200

    fix bug

--- velkysmp/player/akatsuki.lua
+++ velkysmp/player/akatsuki.lua
@@ -2,15 +2,15 @@
 
 function checkForUpdates()
   fs.delete("entdisplay.lua")
-  shell.run("wget https://codeberg.org/Akatsuki/ComputerCraftScripts/raw/commit/main/velkysmp/player/entdisplay.lua")
+  shell.run("wget https://codeberg.org/Akatsuki/ComputerCraftScripts/raw/branch/main/velkysmp/player/entdisplay.lua")
 
   fs.delete("entsense.lua")
-  shell.run("wget https://codeberg.org/Akatsuki/ComputerCraftScripts/raw/commit/main/velkysmp/player/entsense.lua")
+  shell.run("wget https://codeberg.org/Akatsuki/ComputerCraftScripts/raw/branch/main/velkysmp/player/entsense.lua")
 
   fs.delete("meow.lua")
-  shell.run("wget https://codeberg.org/Akatsuki/ComputerCraftScripts/raw/commit/main/velkysmp/player/meow.lua")
+  shell.run("wget https://codeberg.org/Akatsuki/ComputerCraftScripts/raw/branch/main/velkysmp/player/meow.lua")
 
   fs.delete("spinningfish.lua")
-  shell.run("wget https://codeberg.org/Akatsuki/ComputerCraftScripts/raw/commit/main/velkysmp/player/spinningfish.lua")
+  shell.run("wget https://codeberg.org/Akatsuki/ComputerCraftScripts/raw/branch/main/velkysmp/player/spinningfish.lua")
 
 end
