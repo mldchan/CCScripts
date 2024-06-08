301f9f1461bfbf8433cb480cd9e1b99b251b3a5a
Akatsuki2555 <akatsuki@noreply.codeberg.org>
2024-06-08 13:52:33 +0200

    different library for encryption (attempt 43)

--- velkysmp/download.lua
+++ velkysmp/download.lua
@@ -2,7 +2,7 @@ write("What? ")
 local what = read()
 
 if what == "encryption" then
-  shell.run("wget https://www.rjek.com/arcfour.lua.txt arcfour.lua")
+  shell.run("wget https://gist.githubusercontent.com/perara/77b82012bdd2a702c98a714b57e1fb85/raw/ccea5f652cc33a979de02d6e0fe193db0c5bdfb1/aeslua.lua")
 elseif what == "json" then
 	shell.run("wget https://raw.githubusercontent.com/rxi/json.lua/master/json.lua json.lua")
 elseif what == "utils" then
