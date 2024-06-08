2877805d0cf11ec54e7184379337e3914ea28eff
Akatsuki2555 <akatsuki@noreply.codeberg.org>
2024-06-08 19:19:39 +0200

    added broadcasting of encrypted event

--- velkysmp/radarmon/main.lua
+++ velkysmp/radarmon/main.lua
@@ -1,72 +1,91 @@
 -- ComputerCraftScripts
 -- Copyright (C) 2024  Akatsuki
 
--- This program is free software: you can redistribute it and/or modify it under the terms of the 
--- GNU General Public License as published by the Free Software Foundation, either version 3 of 
+-- This program is free software: you can redistribute it and/or modify it under the terms of the
+-- GNU General Public License as published by the Free Software Foundation, either version 3 of
 -- the License, or (at your option) any later version.
 
--- This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without 
--- even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+-- This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+-- even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 -- GNU General Public License for more details.
 
--- You should have received a copy of the GNU General Public License along with this program. 
+-- You should have received a copy of the GNU General Public License along with this program.
 -- If not, see <https://www.gnu.org/licenses/>.
 
+require("aeslua")
 require("utils")
 local json = require("json")
 local config = json.decode(readFile("config.json"))
 
 radar = peripheral.wrap("back") -- Radar peripheral
 
+rednet.open("left")
+
 local playersKnown = {} -- Names of players currently known
 
 while true do
-    os.queueEvent("tesuto")
-    event, p1, p2, p3, p4, p5 = os.pullEventRaw()
+	os.queueEvent("tesuto")
+	event, p1, p2, p3, p4, p5 = os.pullEventRaw()
 
-    local playersScanned = radar.getPlayers()
+	local playersScanned = radar.getPlayers()
 
-    -- Add players not known to playersKnown, the format is [{distance, name}]
+	-- Add players not known to playersKnown, the format is [{distance, name}]
 
-    for _, player in ipairs(playersScanned) do
-        local playerKnown = false
-        for _, known in ipairs(playersKnown) do
-            if known == player.name then
-                playerKnown = true
-                break
-            end
-        end
+	for _, player in ipairs(playersScanned) do
+		local playerKnown = false
+		for _, known in ipairs(playersKnown) do
+			if known == player.name then
+				playerKnown = true
+				break
+			end
+		end
 
-        if not playerKnown then
-            table.insert(playersKnown, player.name)
-            http.post(config.webhook, json.encode({
-                content = "Player " .. player.name .. " was detected at base!",
-                username = "Radar"
-            }), {
-                ["Content-Type"] = "application/json"
-            })
-        end
-    end
+		if not playerKnown then
+			table.insert(playersKnown, player.name)
+			http.post(
+				config.webhook,
+				json.encode({
+					content = "Player " .. player.name .. " was detected at base!",
+					username = "Radar",
+				}),
+				{
+					["Content-Type"] = "application/json",
+				}
+			)
+      rednet.broadcast(encrypt(config.password, json.encode({
+        action = "enter",
+        player = player.name
+      })))
+		end
+	end
 
-    -- Remove players not scanned from playersKnown
+	-- Remove players not scanned from playersKnown
 
-    for i = #playersKnown, 1, -1 do
-        local playerKnown = false
-        for _, player in ipairs(playersScanned) do
-            if playersKnown[i] == player.name then
-                playerKnown = true
-                break
-            end
-        end
+	for i = #playersKnown, 1, -1 do
+		local playerKnown = false
+		for _, player in ipairs(playersScanned) do
+			if playersKnown[i] == player.name then
+				playerKnown = true
+				break
+			end
+		end
 
-        if not playerKnown then
-            http.post(config.webhook, json.encode({
-                content = "Player " .. playersKnown[i] .. " was no longer detected at base!",
-                username = "Radar"
-            }), {
-                ["Content-Type"] = "application/json"
-            })
-            table.remove(playersKnown, i)
-        end
-    end
+		if not playerKnown then
+			http.post(
+				config.webhook,
+				json.encode({
+					content = "Player " .. playersKnown[i] .. " was no longer detected at base!",
+					username = "Radar",
+				}),
+				{
+					["Content-Type"] = "application/json",
+				}
+			)
+      rednet.broadcast(encrypt(config.password, json.encode({
+        action = "leave",
+        player = player.name
+      })))
+			table.remove(playersKnown, i)
+		end
+	end
 end
--- velkysmp/radarmon/startup.lua
+++ velkysmp/radarmon/startup.lua
@@ -12,8 +12,6 @@
 -- You should have received a copy of the GNU General Public License along with this program. 
 -- If not, see <https://www.gnu.org/licenses/>.
 
-settings.set("shell.allow_disk_startup", false)
-
 require("utils")
 local json = require("json")
 local config = json.decode(readFile("config.json"))
@@ -37,20 +35,19 @@ shell.run("wget https://codeberg.org/Akatsuki/ComputerCraftScripts/raw/branch/ma
 
 shell.run("main.lua")
 
-prettyWrite(term, "Nice try. I see you.")
+-- prettyWrite(term, "Nice try. I see you.")
 
 
-http.post(config.webhook, json.encode({
-    content = "Computer " .. os.getComputerID() .. " (Radar computer) had it's program terminated! <@" .. config.userId .. ">"
-}), {
-    ["Content-Type"] = "application/json"
-})
+-- http.post(config.webhook, json.encode({content = "Computer " .. os.getComputerID() .. " (Radar computer) had it's program terminated! <@" .. config.userId .. ">"
+--}), {
+--    ["Content-Type"] = "application/json"
+--})
 
-prettyWrite(term, "Akatsuki was alerted. Please don't try this again.")
-print()
-prettyWrite(term, "Unless you just wanted to update the program. In that case, please wait a moment.")
-print()
+-- prettyWrite(term, "Akatsuki was alerted. Please don't try this again.")
+-- print()
+-- prettyWrite(term, "Unless you just wanted to update the program. In that case, please wait a moment.")
+-- print()
 
-os.sleep(1)
+-- os.sleep(1)
 
-os.reboot()
+-- os.reboot()
