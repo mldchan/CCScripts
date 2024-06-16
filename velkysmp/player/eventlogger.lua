8089759e21bb45bb53c25acff5cd3174426cdabb
Akatsuki <akatsuki@noreply.codeberg.org>
2024-06-16 14:57:27 +0200

    Changes on Sun Jun 16 02:57:27 PM CEST 2024

--- velkysmp/player/eventlogger.lua
+++ velkysmp/player/eventlogger.lua
@@ -2,12 +2,14 @@
 
 per = peripheral.wrap("back")
 
+rednet.open("left")
+
 while true do
     evt, p1, p2, p3, p4, p5 = os.pullEvent()
-    p1 = p1 or "[1]"
-    p2 = p2 or "[2]"
-    p3 = p3 or "[3]"
-    p4 = p4 or "[4]"
-    p5 = p5 or "[5]"
+    p1 = p1 or "[n]"
+    p2 = p2 or "[n]"
+    p3 = p3 or "[n]"
+    p4 = p4 or "[n]"
+    p5 = p5 or "[n]"
     per.tell(evt .. ": " .. p1 .. " " .. p2 .. " " .. p3 .. " " .. p4 .. " " .. p5)
 end
