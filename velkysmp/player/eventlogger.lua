11768e3fdd5c1ea3f361a1bdeb9a2555d3ceda24
Akatsuki <akatsuki@noreply.codeberg.org>
2024-06-16 14:53:26 +0200

    Changes on Sun Jun 16 02:53:26 PM CEST 2024

--- velkysmp/player/eventlogger.lua
+++ velkysmp/player/eventlogger.lua
@@ -4,5 +4,10 @@ per = peripheral.wrap("back")
 
 while true do
     evt, p1, p2, p3, p4, p5 = os.pullEvent()
+    p1 = p1 or "[1]"
+    p2 = p2 or "[2]"
+    p3 = p3 or "[3]"
+    p4 = p4 or "[4]"
+    p5 = p5 or "[5]"
     per.tell(evt .. ": " .. p1 .. " " .. p2 .. " " .. p3 .. " " .. p4 .. " " .. p5)
 end
