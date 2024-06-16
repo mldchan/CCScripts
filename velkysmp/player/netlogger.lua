55bd28cfb8aebd3dc75f2881552a2e7f403ea25d
Akatsuki <akatsuki@noreply.codeberg.org>
2024-06-16 13:36:17 +0200

    Changes on Sun Jun 16 01:36:17 PM CEST 2024

---
+++ velkysmp/player/netlogger.lua
@@ -0,0 +1,16 @@
+-- ComputerCraft
+
+function appendToFile(log)
+    local logFile = fs.open("log.txt", "a")
+    logFile.writeLine(log)
+    logFile.close()
+end
+
+while true do
+    evt, p1, p2, p3, p4, p5 = os.pullEvent()
+
+    if evt == "rednet_message" then
+        print("Received message from " .. p1 .. ": " .. p3)
+        appendToFile("Received message from " .. p1 .. ": " .. p3)
+    end
+end
