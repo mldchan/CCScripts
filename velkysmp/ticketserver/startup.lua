61330176c67a3c59ff4ff230f76b2fe1ac8b97fb
Akatsuki <akatsuki@noreply.codeberg.org>
2024-06-16 13:11:30 +0200

    Changes on Sun Jun 16 01:11:30 PM CEST 2024

--- velkysmp/ticketserver/startup.lua
+++ velkysmp/ticketserver/startup.lua
@@ -20,5 +20,7 @@ fs.delete("json.lua")
 shell.run("wget https://raw.githubusercontent.com/rxi/json.lua/master/json.lua json.lua")
 fs.delete("main.lua")
 shell.run("wget https://codeberg.org/Akatsuki/ComputerCraftScripts/raw/branch/main/velkysmp/ticketserver/main.lua main.lua")
+fs.delete("utils.lua")
+shell.run("wget https://codeberg.org/Akatsuki/ComputerCraftScripts/raw/branch/main/velkysmp/utils.lua utils.lua")
 
 shell.run("main.lua")
