d593c424f131aa4f2b2beb74023fcf50764691c4
Akatsuki2555 <akatsuki2555@proton.me>
2024-06-23 22:00:02 +0200

    Add computer starter

---
+++ velkysmp/starter/startup.lua
@@ -0,0 +1,72 @@
+-- ComputerCraftScripts
+-- Copyright (C) 2024  Akatsuki
+
+-- This program is free software: you can redistribute it and/or modify it under the terms of the 
+-- GNU General Public License as published by the Free Software Foundation, either version 3 of 
+-- the License, or (at your option) any later version.
+
+-- This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without 
+-- even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+-- GNU General Public License for more details.
+
+-- You should have received a copy of the GNU General Public License along with this program. 
+-- If not, see <https://www.gnu.org/licenses/>.
+
+-- wget run https://raw.githubusercontent.com/Akatsuki2555/CCScripts/main/velkysmp/starter/startup.lua
+
+function downloadFile(f, l)
+    print(f .. ": downloading update..")
+    -- Get string
+    local startupUpdate, errorMessage = http.get(l)
+    if startupUpdate == nil then
+        printError("Error while trying to download file " .. errorMessage)
+    end
+    local status, message = startupUpdate.getResponseCode()
+    if status ~= 200 then
+        printError("Server responded with message " .. message)
+        return
+    end
+
+    local fileC = startupUpdate.readAll()
+
+    print(f .. ": deleting...")
+    -- Delete the startup file
+    fs.delete(f .. ".lua")
+
+    print(f .. ": replacing..")
+    -- Recreate the startup file
+    local file = fs.open(f .. ".lua", "w")
+    file.write(fileC)
+    file.close()
+    print(f .. ": done updating.")
+end
+
+downloadFile("startup", "https://raw.githubusercontent.com/Akatsuki2555/CCScripts/main/velkysmp/starter/startup.lua")
+downloadFile("json", "https://raw.githubusercontent.com/rxi/json.lua/master/json.lua")
+downloadFile("utils", "https://raw.githubusercontent.com/Akatsuki2555/CCScripts/main/velkysmp/utils.lua")
+
+local function turnOnAllComputers()
+    print("Turning on all computers...")
+    
+    local computers = {}
+    
+    -- Get a list of all connected peripherals
+    local peripherals = peripheral.getNames()
+    
+    -- Filter out the computers
+    for _, peripheralName in ipairs(peripherals) do
+        if peripheral.getType(peripheralName) == "computer" then
+            table.insert(computers, peripheral.wrap(peripheralName))
+        end
+    end
+    
+    -- Call the "turnOn" function on each computer
+    for _, computer in ipairs(computers) do
+        computer.turnOn()
+        print("Computer turned on: " .. computer.getID())
+    end
+    
+    print("All computers turned on.")
+end
+
+turnOnAllComputers()
