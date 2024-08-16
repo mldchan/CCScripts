70bdaf65a95017ae5af9994d54ac8020c7cf89c7
Akatsuki2555 <akatsuki2555@proton.me>
2024-08-17 01:06:23 +0200

    Format and fix computer starter script

--- velkysmp/starter/main.lua
+++ velkysmp/starter/main.lua
@@ -1,39 +1,39 @@
 -- ComputerCraftScripts
 -- Copyright (C) 2024  Akatsuki
 
--- This program is free software: you can redistribute it and/or modify it under the terms of the 
--- GNU General Public License as published by the Free Software Foundation, either version 3 of 
+-- This program is free software: you can redistribute it and/or modify it under the terms of the
+-- GNU General Public License as published by the Free Software Foundation, either version 3 of
 -- the License, or (at your option) any later version.
 
--- This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without 
--- even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+-- This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+-- even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 -- GNU General Public License for more details.
 
--- You should have received a copy of the GNU General Public License along with this program. 
+-- You should have received a copy of the GNU General Public License along with this program.
 -- If not, see <https://www.gnu.org/licenses/>.
 
 local function scanNetwork()
     print("Scanning network for offline computers...")
-    
+
     local computers = {}
-    
+
     -- Get a list of all connected peripherals
     local peripherals = peripheral.getNames()
-    
+
     -- Filter out the computers
     for _, peripheralName in ipairs(peripherals) do
         if peripheral.getType(peripheralName) == "computer" then
             table.insert(computers, peripheral.wrap(peripheralName))
         end
     end
-    
+
     -- Check the status of each computer
     for _, computer in ipairs(computers) do
         if not computer.isOn() then
             print("Computer offline: " .. computer.getID())
         end
     end
-    
+
     print("Network scan complete.")
 end
 
@@ -41,35 +41,29 @@ scanNetwork()
 
 local function turnOnAllComputers()
     print("Turning on all computers...")
-    
+
     local computers = {}
-    
+
     -- Get a list of all connected peripherals
     local peripherals = peripheral.getNames()
-    
+
     -- Filter out the computers
     for _, peripheralName in ipairs(peripherals) do
         if peripheral.getType(peripheralName) == "computer" then
             table.insert(computers, peripheral.wrap(peripheralName))
         end
     end
-    
+
     -- Check the status of each computer and turn it on if it's offline
     for _, computer in ipairs(computers) do
         if not computer.isOn() then
             computer.turnOn()
             print("Computer turned on: " .. computer.getID())
-            
-            http.post("https://akatsuki.nekoweb.org/webhook", json.encode({
-                content = "Computer " .. computer.getID() .. " was offline and was turned on."
-            }), {
-                ["Content-Type"] = "application/json"
-            })
         else
             print("Computer already online: " .. computer.getID())
         end
     end
-    
+
     print("All computers turned on.")
 end
 
