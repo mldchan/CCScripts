903041f4aefc6c1e35db21f521d854834218fda0
Akatsuki2555 <akatsuki2555@proton.me>
2024-08-17 01:05:04 +0200

    Update VelkySMP monitor with colors on screen

--- velkysmp/spawnmon/main.lua
+++ velkysmp/spawnmon/main.lua
@@ -13,7 +13,7 @@
 -- If not, see <https://www.gnu.org/licenses/>.
 
 
-VERSION = "1.08"
+VERSION = "1.1"
 
 require("utils")
 local json = require("json")
@@ -24,103 +24,32 @@ config = json.decode(config_file)
 local mon = peripheral.wrap(config.side)
 
 if config.text_scale ~= nil then
-  mon.setTextScale(config.text_scale)
+    mon.setTextScale(config.text_scale)
 end
 
 local mon_width, mon_height = mon.getSize()
 
-local setting_show_online_first = false
 local has_loaded = false
 local api_parsed = {}
-local show_settings = false
-local small_text = false
 
 function drawMainScreen()
-  if show_settings then
-    -- Create background
-    mon.setBackgroundColor(colors.white)
-
-    mon.setCursorPos(5, 5)
-    mon.write("                              ")
-    mon.setCursorPos(5, 6)
-    mon.write("                              ")
-    mon.setCursorPos(5, 7)
-    mon.write("                              ")
-    mon.setCursorPos(5, 8)
-    mon.write("                              ")
-    mon.setCursorPos(5, 9)
-    mon.write("                              ")
-    mon.setCursorPos(5, 10)
-    mon.write("                              ")
-
-    mon.setTextColor(colors.black)
-
-    mon.setCursorPos(6, 4)
-    mon.write("Settings")
-
-    mon.setCursorPos(6, 6)
-    mon.write("[")
-
-    mon.setTextColor(colors.orange)
-
-    if setting_show_online_first then
-      mon.write("X")
-    else
-      mon.write(" ")
-    end
-
-    mon.setTextColor(colors.black)
-
-    mon.write("] Show online players first")
-
-    mon.setCursorPos(6, 7)
-    mon.write("[")
-
-    mon.setTextColor(colors.orange)
-
-    if small_text then
-      mon.write("X")
-    else
-      mon.write(" ")
-    end
-
-    mon.setTextColor(colors.black)
-
-    mon.write("] Smaller text")
-
-    mon.setCursorPos(6, 9)
-    mon.setTextColor(colors.orange)
-    mon.write("[ Close ]")
-    mon.setTextColor(colors.white)
-    mon.setBackgroundColor(colors.black)
-  else
     if not has_loaded then
-      local api_content = http.get("https://velkysmp-mon.vercel.app/api/get").readAll()
-      api_parsed = json.decode(api_content)
-      has_loaded = true
+        local api_content = http.get("https://velkysmp-mon.vercel.app/api/get").readAll()
+        api_parsed = json.decode(api_content)
+        has_loaded = true
     end
     local api_sorted = {}
 
-    if setting_show_online_first then
-      for k, i in ipairs(api_parsed.players) do
+    for k, i in ipairs(api_parsed.players) do
         if i.online then
-          table.insert(api_sorted, i)
+            table.insert(api_sorted, i)
         end
-      end
-
-      for k, i in ipairs(api_parsed.players) do
-        if not i.online then
-          table.insert(api_sorted, i)
-        end
-      end
-    else
-      api_sorted = api_parsed.players
     end
 
-    if small_text then
-      mon.setTextScale((config.text_scale or 1) / 2)
-    else
-      mon.setTextScale(config.text_scale or 1)
+    for k, i in ipairs(api_parsed.players) do
+        if not i.online then
+            table.insert(api_sorted, i)
+        end
     end
 
     mon_width, mon_height = mon.getSize()
@@ -129,32 +58,33 @@ function drawMainScreen()
     mon.setCursorPos(1, 1)
     prettyWrite(mon, "Akatsuki's VelkySMP monitor - since 2024/02/25!")
 
-    mon.setCursorPos(1, 3)
-    prettyWrite(mon, "Top players by online and playtime")
-
-    local items_count = mon_height - 5
+    local items_count = mon_height - 3
     for k, v in ipairs(api_sorted) do
-      if k > items_count then
-        break
-      end
+        if k > items_count then
+            break
+        end
 
-      mon.setCursorPos(2, 4 + k)
-      mon.write(k .. ". " .. v.name)
-      mon.setCursorPos(mon_width - 20, 4 + k)
-      mon.write(v.humantime)
+        color = v.profileStyle.ccColor or "white"
 
-      if v.online then
-        mon.setCursorPos(mon_width - 5, 4 + k)
-        mon.write("Online")
-      end
+        mon.setCursorPos(2, 2 + k)
+        table.foreach(colors, function(k, v)
+            if type(v) == "number" and k == color then
+                mon.setTextColor(v)
+            end
+        end)
+        mon.write(k .. ". " .. v.name)
+        mon.setTextColor(colors.white)
+        mon.setCursorPos(mon_width - 20, 2 + k)
+        mon.write(v.humantime)
+
+        if v.online then
+            mon.setCursorPos(mon_width - 5, 2 + k)
+            mon.write("Online")
+        end
     end
 
-    mon.setCursorPos(1, mon_height)
-    mon.write("Akatsuki ComputerCraft Monitor v" .. VERSION)
-
-    mon.setCursorPos(40, mon_height)
-    mon.write("Settings")
-  end
+    mon.setCursorPos(mon_width - #"Version " - #VERSION + 1, mon_height)
+    mon.write("Version " .. VERSION)
 end
 
 mon.clear()
@@ -169,107 +99,43 @@ mon.write("Refreshing... this may take a while")
 
 drawMainScreen()
 
-rednet.open(config.modemSide)
-rednet.host("Akatsuki", config.hostname)
+timer = os.startTimer(30)
 
-local knownComputers = {}
-local computerMsgsStatus = {}
+timer2 = os.startTimer(3600)
 
 while true do
-  event, p1, p2, p3, p4, p5 = os.pullEventRaw()
-  if event == "peripheral_detach" then
-    -- p1 - side, p2 - type
-    http.post(config.webhook, json.encode({
-      content = "Peripheral " .. p1 .. " was detached! <@" .. config.userId .. ">"
-    }), {
-      ["Content-Type"] = "application/json"
-    })
-  end
-
-  if event == "peripheral" then
-    -- p1 - side, p2 - type
-    http.post(config.webhook, json.encode({
-      content = "Peripheral " .. p1 .. " was attached! <@" .. config.userId .. ">"
-    }), {
-      ["Content-Type"] = "application/json"
-    })
-  end
-
-  if event == "timer" then
-    -- p1 - timer id
-    if timer == p1 then
-      for index, value in pairs(computerMsgsStatus) do
-        print("Computer " .. index .. " status: " .. value)
-        if value == "sent" then
-          http.post(config.webhook, json.encode({
-            content = "Computer " ..
-                index .. " did not respond when sent from " .. os.getComputerID() .. "! <@" .. config.userId .. ">"
-          }), {
-            ["Content-Type"] = "application/json"
-          })
-        end
-        -- unregister computer
-        print("Unregistering computer " .. index)
-        computerMsgsStatus[index] = nil
-      end
-
-      rednet.broadcast("ping", "Akatsuki")
-      -- set all known computers to "Sent"
-      for index, value in pairs(knownComputers) do
-        computerMsgsStatus[index] = "sent"
-      end
-
-      timer = os.startTimer(5)
-    end
-  end
-
-  if event == "monitor_touch" then
-    if p2 > 40 and p2 < 47 and p3 == mon_height then
-      show_settings = not show_settings
-      print("show settings")
-      drawMainScreen()
-    end
-
-    if show_settings then
-      if p2 == 7 and p3 == 6 then
-        setting_show_online_first = not setting_show_online_first
-        drawMainScreen()
-      end
-
-      if p2 == 7 and p3 == 7 then
-        small_text = not small_text
-        drawMainScreen()
-      end
-
-      if p2 >= 6 and p2 <= 13 and p3 == 9 then
-        show_settings = false
-        drawMainScreen()
-      end
-    end
-  elseif event == "monitor_resize" then
-    drawMainScreen()
-  end
-
-  if event == "rednet_message" then
-    -- p1 - sender id, p2 - message, p3 - protocol
-    if p2 == "ping" and p3 == "Akatsuki" then
-      rednet.send(p1, "pong", "Akatsuki")
-      print("Ping response sent to " .. tostring(p1))
-    end
-
-    if p2 == "pong" and p3 == "Akatsuki" then
-      print("Received pong from " .. tostring(p1))
-      computerMsgsStatus[p1] = "received"
-      -- add to list of known computers if not already there
-      if not knownComputers[p1] then
-        print("Registering new known computer " .. tostring(p1))
-        knownComputers[p1] = true
+    event, p1, p2, p3, p4, p5 = os.pullEventRaw()
+    if event == "peripheral_detach" then
+        -- p1 - side, p2 - type
         http.post(config.webhook, json.encode({
-          content = "Computer " .. p1 .. " has connected from " .. os.getComputerID() .. "!"
+            content = "Peripheral " .. p1 .. " was detached! <@" .. config.userId .. ">"
         }), {
-          ["Content-Type"] = "application/json"
+            ["Content-Type"] = "application/json"
         })
-      end
     end
-  end
+
+    if event == "peripheral" then
+        -- p1 - side, p2 - type
+        http.post(config.webhook, json.encode({
+            content = "Peripheral " .. p1 .. " was attached! <@" .. config.userId .. ">"
+        }), {
+            ["Content-Type"] = "application/json"
+        })
+    end
+
+    if event == "monitor_resize" then
+        drawMainScreen()
+    end
+
+    if event == "timer" and p1 == timer then
+        print("Refresh screen after 30 seconds")
+        drawMainScreen()
+        timer = os.startTimer(30)
+    elseif event == "timer" and p1 == timer2 then
+        print("Full refresh after 1 hour")
+        local api_content = http.get("https://velkysmp-mon.vercel.app/api/get").readAll()
+        api_parsed = json.decode(api_content)
+        drawMainScreen()
+        timer2 = os.startTimer(3600)
+    end
 end
